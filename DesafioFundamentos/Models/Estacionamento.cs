8a752f31b1796b8e3e9f5df9a68b4ac3877fa3f8
rafaScalet <rafaelscalet2018@gmail.com>
2024-05-02 02:05:15 -0300

    feat: 'ListarVeiculo' deployed

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -46,8 +46,9 @@ namespace DesafioFundamentos.Models
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                foreach(string veiculo in veiculos){
+                    Console.WriteLine(veiculo);
+                }
             }
             else
             {
